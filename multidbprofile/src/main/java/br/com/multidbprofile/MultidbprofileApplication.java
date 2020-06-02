2e313e258cc4fc83e5b14b6f1e088d629d34d01d
Miqueias Sousa <mi.queias2010@hotmail.com>
2020-06-02 08:26:57 -0300

    POC de conexão para multiplos SGDBs e multiplos profiles

---
+++ multidbprofile/src/main/java/br/com/multidbprofile/MultidbprofileApplication.java
@@ -0,0 +1,13 @@
+package br.com.multidbprofile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultidbprofileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultidbprofileApplication.class, args);
+	}
+
+}
---
+++ multidbprofile/src/test/java/br/com/multidbprofile/MultidbprofileApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.multidbprofile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultidbprofileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
